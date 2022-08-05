e300fea1a7163a41962bab5af2fd1b5434e32dfc
Varun E R <varunercruz@gmail.com>
2022-08-05 17:39:31 +0530

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,9 +3,14 @@ function App(){
 
 
 function counter(){
-  count+=1
-  console.log(count);
-  }
+    count+=1
+    console.log(count);
+    }
+return(
+<div>
 <button onClick={counter}>Click here</button>
-}
+<h1> hello world</h1>
+</div>
+)
+};
 export default App;
